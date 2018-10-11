a2bca8a123087c238f617dd9fe00b9be8324ab24
mistikk <mustafa-buyukcelebi@hotmail.com>
2018-10-11 01:11:09 -0400

    testing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-require("./src/index");
+import React, { Component } from "react";
+import { AppRegistry, View, Text } from "react-native";
+const Realm = require("realm");
+
+const app = class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { realm: null };
+    }
+
+    componentWillMount() {
+        Realm.open({
+            schema: [{ name: "Dog", properties: { name: "string" } }],
+        }).then(realm => {
+            realm.write(() => {
+                realm.create("Dog", { name: "Rex" });
+            });
+            this.setState({ realm });
+        });
+    }
+
+    render() {
+        const info = this.state.realm
+            ? "Number of dogs in this Realm: " +
+              this.state.realm.objects("Dog").length
+            : "Loading...";
+
+        return (
+            <View>
+                <Text>{info}</Text>
+            </View>
+        );
+    }
+};
+AppRegistry.registerComponent("eSteem", () => app);
