c0bd48268218dff103acaa09559e2b5f531a1a9a
mistikk <mustafa-buyukcelebi@hotmail.com>
2018-10-12 23:26:58 -0400

    removed src for testing

--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import accountReducer from "./accountReducer";
-
-export default combineReducers({
-    account: accountReducer,
-});
