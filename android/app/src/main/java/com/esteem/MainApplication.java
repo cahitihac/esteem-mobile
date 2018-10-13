d746fed2c018e798d22bed5c24be08394399bd5a
mistikk <mustafa-buyukcelebi@hotmail.com>
2018-10-12 23:12:21 -0400

    update

--- android/app/src/main/java/com/esteem/MainApplication.java
+++ android/app/src/main/java/com/esteem/MainApplication.java
@@ -3,11 +3,9 @@ package com.esteem;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.avishayil.rnrestart.ReactNativeRestartPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
+import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,11 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RealmReactPackage(),
             new VectorIconsPackage(),
-            new ReactNativeRestartPackage(),
-            new LinearGradientPackage(),
-            new FastImageViewPackage()
+            new FastImageViewPackage(),
+            new RealmReactPackage()
       );
     }
 
