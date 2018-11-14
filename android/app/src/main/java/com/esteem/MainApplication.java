a903d64f12f9771ecf00e9647e08f9691487a656
Feruz <fmiboy@gmail.com>
2018-11-14 15:59:45 +0200

    firebase package link

--- android/app/src/main/java/com/esteem/MainApplication.java
+++ android/app/src/main/java/com/esteem/MainApplication.java
@@ -3,6 +3,7 @@ package com.esteem;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import io.realm.react.RealmReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new FastImageViewPackage(),
             new RealmReactPackage(),
